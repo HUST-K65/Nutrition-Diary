01e3a5ca5aa9ecc92e8de9a7525d23c5c718c1e4
TheHuy <thehuy161802@gmail.com>
2024-01-08 01:04:08 +0700

    Revert "update"
    This reverts commit de6f01c21de3c005c062178fa82f750ecefc0c0a.

--- Source/client/src/components/ControlBarComponent.js
+++ Source/client/src/components/ControlBarComponent.js
@@ -124,13 +124,7 @@ export default function ControlBar({ indexComponentActive, setIndexComponentActi
                                                 </View>
                                                 <Text className="text-lg text-white">Weight</Text>
                                             </TouchableOpacity>
-                                            <TouchableOpacity
-                                                className="items-center"
-                                                onPress={() => {
-                                                    setOpenModalPlus(false);
-                                                    navigation.navigate("NewGoal", { isNew: true, setIndexComponentActive: setIndexComponentActive })
-                                                }}
-                                            >
+                                            <TouchableOpacity className="items-center">
                                                 <View className="bg-sky-400 rounded-full p-2">
                                                     <AntDesign name="plus" size={30} style={{ color: "#09508a" }} />
                                                 </View>
--- Source/client/src/components/log/LogComponent.js
+++ Source/client/src/components/log/LogComponent.js
@@ -1,114 +1,19 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { ScrollView, Text, View } from 'react-native'
 import ExerciseLogComponent from './exercise/ExerciseLogComponent'
 import MealLogComponent from './meal/MealLogComponent'
 import HealthLogComponent from './health/HealthLogComponent'
-import { useIsFocused } from '@react-navigation/native'
-
-const caloriesPerKg = 7500;
-
-async function getExercises(setExercises) {
-
-    await fetch("http://10.0.2.2:8000/api/nutrition_diary/v1/exercise/getUserExercises", {
-        method: "post",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            "userId": "659a676eaf291d973da3758b",
-            "date": 1704585600000
-        })
-    }
-    ).then(async (response) => {
-        const res = await response.json();
-        if (res && res.data && res.data.length) {
-            setExercises(res.data)
-        }
-    })
-        .catch(function (error) {
-            Alert.alert(
-                'Error',
-                error.message,
-                [
-                    {
-                        text: 'Cancel',
-                        style: 'cancel',
-                    },
-                ],
-                {
-                    cancelable: true,
-                },
-            );
-        });
-}
-
-async function getCurrentGoal(setCurrentGoal, setCurrentWeight) {
-    await fetch("http://10.0.2.2:8000/api/nutrition_diary/v1/goal/getGoal", {
-        method: "post",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            "userId": "659a676eaf291d973da3758b"
-        })
-    }
-    ).then(async (response) => {
-        const res = await response.json();
-        console.log(res)
-        if (res && res.data && res.data.length) {
-            setCurrentGoal(res.data[0].currentGoal)
-            setCurrentWeight(res.data[0].currentWeight);
-
-        }
-    })
-        .catch(function (error) {
-            Alert.alert(
-                'Error',
-                error.message,
-                [
-                    {
-                        text: 'Cancel',
-                        style: 'cancel',
-                    },
-                ],
-                {
-                    cancelable: true,
-                },
-            );
-        });
-}
-
 
 export default function LogComponent() {
-    const isFocused = useIsFocused();
-    const [exercises, setExercises] = useState([]);
-    const [currentGoal, setCurrentGoal] = useState(0);
-    const [currentWeight, setCurrentWeight] = useState(0);
 
-    useEffect(() => {
-        getCurrentGoal(setCurrentGoal, setCurrentWeight)
-        getExercises(setExercises);
-    }, [isFocused])
-
-    let budget = Math.ceil((currentGoal - currentWeight) * caloriesPerKg / 30);
-    let totalCaloriesExercise = 0;
-
-    exercises.map(item => {
-        totalCaloriesExercise += (item.userCalories ? item.userCalories : item.exercise.calories)
-    })
-
-    totalCaloriesExercise = Math.ceil(totalCaloriesExercise);
-
-    let net = 122 - totalCaloriesExercise;
-
-    let underPositive = (budget - net) > 0;
+    let underPositive = true;
     let classTextUnder = (underPositive ? "bg-green-600 " : "bg-red-500") + " text-lg text-white";
     return (
         <ScrollView className="mb-20" showsVerticalScrollIndicator={false}>
             <View className="flex-1 items-center justify-center flex-row space-x-8 space-y-0 bg-white border-2 border-gray-200 p-3">
                 <View className="items-center">
                     <Text className="text-gray-800">Budget</Text>
-                    <Text className="font-bold text-lg text-blue-800">{budget}</Text>
+                    <Text className="font-bold text-lg text-blue-800">2.322</Text>
                 </View>
                 <View>
                     <Text className="text-gray-800">Food</Text>
@@ -116,15 +21,15 @@ export default function LogComponent() {
                 </View>
                 <View className="items-center">
                     <Text className="text-gray-800">Exercise</Text>
-                    <Text className="font-bold text-lg">-{totalCaloriesExercise}</Text>
+                    <Text className="font-bold text-lg">0</Text>
                 </View>
                 <View>
                     <Text className="text-gray-800">Net</Text>
-                    <Text className="font-bold text-lg">{net}</Text>
+                    <Text className="font-bold text-lg">122</Text>
                 </View>
                 <View>
                     <Text className="text-gray-800">Under</Text>
-                    <Text className={classTextUnder}>{budget - net}</Text>
+                    <Text className={classTextUnder}>2.200</Text>
                 </View>
             </View>
 
@@ -133,7 +38,7 @@ export default function LogComponent() {
 
             <Text className="text-2xl m-4">Exercise</Text>
 
-            <ExerciseLogComponent exercises={exercises} totalCaloriesExercise={totalCaloriesExercise} />
+            <ExerciseLogComponent />
 
             <Text className="text-2xl m-4">Health</Text>
             <HealthLogComponent />
